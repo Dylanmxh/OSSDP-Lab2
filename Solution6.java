211e9484db264f7c3918238db30b4e8ae9acfe93
马三333 <maxinhua0607@163.com>
2024-10-12 20:35:54 +0800

    Added L2022211862_6_Test.java and modified Solution6.java

--- Solution6.java
+++ Solution6.java
@@ -45,39 +45,47 @@ class Solution6 {
     Set<String>[] s = new Set[105];
 
     public List<Integer> peopleIndexes(List<List<String>> favoriteCompanies) {
-        for (int i = 1; i < 105; ++i) {
-            s[i] = new HashSet<String>();
+        // 初始化每个用户的收藏公司集合
+        for (int i = 0; i < 105; ++i) {
+            s[i] = new HashSet<>();
         }
-        int n = favoriteCompanies.size()-1;
-        List<Integer> ans = new ArrayList<Integer>();
+        int n = favoriteCompanies.size();
+        List<Integer> ans = new ArrayList<>();
 
+        // 将每个用户的公司列表转为 Set 存储，便于后续查找
         for (int i = 0; i < n; ++i) {
             for (String com : favoriteCompanies.get(i)) {
                 s[i].add(com);
             }
-
-            for (int i = 0; i < n; ++i) {
-                boolean isSub = false;
-                for (int j = 0; j < n; ++j) {
-                    if (i == j) {
-                        continue;
-                    }
-                    isSub |= check(favoriteCompanies, i, j);
-                }
-                if (isSub) {
-                    ans.add(i);
-                }
-            }
-
-            return ans;
         }
 
-        public boolean check(List<List<String>> favoriteCompanies, int x, int y) {
-            for (String com : favoriteCompanies.get(x)) {
-                if (!s[y].contains(com)) {
-                    return false;
+        // 检查每个用户是否是其他用户的子集
+        for (int i = 0; i < n; ++i) {
+            boolean isSub = false;
+            for (int j = 0; j < n; ++j) {
+                if (i == j) {
+                    continue;
+                }
+                if (check(favoriteCompanies, i, j)) {
+                    isSub = true;
+                    break;
                 }
             }
-            return true;
+            if (!isSub) {
+                ans.add(i);
+            }
         }
+
+        return ans;
     }
+
+    // 检查 favoriteCompanies[i] 是否是 favoriteCompanies[j] 的子集
+    private boolean check(List<List<String>> favoriteCompanies, int x, int y) {
+        for (String com : favoriteCompanies.get(x)) {
+            if (!s[y].contains(com)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
